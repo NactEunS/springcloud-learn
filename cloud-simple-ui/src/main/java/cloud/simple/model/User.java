34112f69c24f0175cded95ffacaa05740df594d2
NactEunS <eweiyan05947809@126.com>
2021-04-10 18:04:27 +0800

    feate: 服务注册方(cloud-eureka-server),服务提供方(cloud-simple-service),服务消费方(cloud-simple-ui),配置服务(cloud-config-server),配置仓库(cloud-config-repo)

---
+++ cloud-simple-ui/src/main/java/cloud/simple/model/User.java
@@ -0,0 +1,15 @@
+
+package cloud.simple.model;
+
+public class User {
+
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
